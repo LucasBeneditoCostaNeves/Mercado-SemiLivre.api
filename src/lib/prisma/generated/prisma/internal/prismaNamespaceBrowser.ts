8ff8b92fb7cea952fe32086cf8c25bbdb07e3152
Lucas <110197724+LucasBeneditoCostaNeves@users.noreply.github.com>
2026-03-28 18:16:33 -0300

    Feat: Add column name in Model User

--- src/lib/prisma/generated/prisma/internal/prismaNamespaceBrowser.ts
+++ src/lib/prisma/generated/prisma/internal/prismaNamespaceBrowser.ts
@@ -72,6 +72,7 @@ export type TransactionIsolationLevel = (typeof TransactionIsolationLevel)[keyof
 
 export const UserScalarFieldEnum = {
   id: 'id',
+  nome: 'nome',
   email: 'email',
   password: 'password',
   createdAt: 'createdAt',
