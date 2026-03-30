105c86c86e404abf78bf024090c43b685117138f
Lucas Benedito <lucasbene03@gmail.com>
2026-03-30 16:34:26 -0300

    Add Prisma client, SQLite adapter and user module
    Add generated Prisma client and runtime artifacts, update prisma schema, and add database infra (Prisma service, mappers, repository, and module). Introduce HTTP user module with controller, DTOs and view model; update app.module.ts and main.ts to wire services. Add dependencies for Prisma SQLite adapter (better-sqlite3) and validation (class-transformer, class-validator), update package-lock/yarn.lock, and enable Prisma formatting in VSCode settings.

---
+++ src/infra/database/prisma/repositories/PrismaUserReposiory.ts
@@ -0,0 +1,20 @@
+import { User } from "src/modules/user/entities/User";
+import { UserRepository } from "src/modules/user/reposiories/UserRepository";
+import { PrismaUserMapper } from "../mappers/PrismaUserMapper";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+
+    constructor(private prisma: PrismaService) { }
+
+    async create(user: User): Promise<void> {
+
+        const userRaw = PrismaUserMapper.toPrisma(user)
+
+        await this.prisma.user.create({
+            data: userRaw
+        })
+    }
+}
---
+++ src/infra/http/modules/user/dtos/createUserBody.ts
@@ -0,0 +1,24 @@
+import { IsBoolean, IsEmail, isNotEmpty, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserBody {
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5, {
+        message: 'Password is too short',
+    })
+    password: string;
+
+
+    @IsBoolean()
+    @IsNotEmpty()
+    status: boolean;
+}
