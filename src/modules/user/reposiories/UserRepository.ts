5802fbecbbc29165bbc1cb115e995a88f6611cb1
Lucas <150089160+LucasIFollow@users.noreply.github.com>
2026-04-11 14:47:56 -0300

    Fix: tipes and interfaces

--- src/modules/user/reposiories/UserRepository.ts
+++ src/modules/user/reposiories/UserRepository.ts
@@ -1,7 +1,19 @@
-import { User } from "../entities/User";
+import { User } from "../entities/User"
+
+export interface IUserDTO {
+    id: string
+    name: string
+    email: string
+    password: string
+    status: boolean
+    profileId: string
+    createdAt: Date
+    updatedAt: Date
+}
+
 
 export abstract class UserRepository {
-    abstract create(user: User): Promise<void>;
+    abstract create(user: User): Promise<void>
     abstract findByEmail(email: string): Promise<User | null>
-    abstract listManyUser(): Promise<any>
+    abstract listManyUser(): Promise<IUserDTO[] | null>
 }
